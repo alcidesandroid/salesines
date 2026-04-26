40a48b47a7b50fe3cd32e453be776bff2d96f755
Alcides <66286436+alcidesandroid@users.noreply.github.com>
2026-04-25 20:15:37 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://qafhbbrkpqkgcqxntdma.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhZmhiYnJrcHFrZ2NxeG50ZG1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxNDUxNjQsImV4cCI6MjA5MjcyMTE2NH0.VHmgTAVs9_k_eTLqSmJpadSEgew-KJBwcGTWxdE1egI"
+};
